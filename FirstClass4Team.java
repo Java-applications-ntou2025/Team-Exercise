875c9f99aa16e06d3d622b22e04fc8b647d440ad
Tony9737 <t0910046666@gmail.com>
2025-04-21 09:38:08 +0800

    version 3

--- FirstClass4Team.java
+++ FirstClass4Team.java
@@ -3,5 +3,6 @@ public class FirstClass4Team {
         System.out.println("這是用來練習GitHub協作的程式!");
         System.out.println("\"學號：\"01257131\"\n姓名：\"王鈞宇\"");
          System.out.println("\"學號：\"01257164\"\n姓名：\"劉長諺\"");
+         System.out.println("\"學號：\"01257111\"\n姓名：\"翁世華\"");
      }
  }
